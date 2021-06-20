bd18efd22543594d039350dadb2467c2e423da88
Anisse Astier <anisse@astier.eu>
2021-06-20 23:31:40 +0200

    add ability to run code in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 mod disas;
 mod cpu;
 
+use std::env;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
 pub fn main() {
-    
+    let args: Vec<String> = env::args().collect();
+    let file = args.get(1).expect("needs an argument");
+    let path = Path::new(file);
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Cannot open {}: {}", path.display(), why),
+        Ok(f) => f,
+    };
+
+    let mut data: Vec<u8> = vec!();
+    if let Err(why) = file.read_to_end(&mut data) {
+        panic!("Could not read {}: {}", path.display(), why);
+    }
+
+    let mut cpu = cpu::init();
+    cpu::run(&mut cpu, &data)
 }
