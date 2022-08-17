1bdbbc19837d6fbff13f15c9d5ddf42bbd63eb65
Anisse Astier <anisse@astier.eu>
2022-08-17 22:54:16 +0200

    mem: attempt to make it faster with different implementation

--- src/mem.rs
+++ src/mem.rs
@@ -2,8 +2,9 @@ use std::fmt;
 
 #[derive(PartialEq, Clone, Debug)]
 enum Dest {
-    ROM { start: u16, end: u16 },
-    RAM,
+    ROM { start: u16 },
+    RAM { start: u16 },
+    Panic,
 }
 
 pub enum Mapper {
@@ -12,18 +13,11 @@ pub enum Mapper {
     // Leave possibility to add codemasters mapper later
 }
 
-#[derive(PartialEq, Clone, Debug)]
-struct Mapping {
-    start: u16,
-    end: u16,
-    dest: Dest,
-}
-
 #[derive(PartialEq, Clone)]
 pub struct Memory {
     ram: Vec<u8>,
     rom: Vec<u8>,
-    mappings: Vec<Mapping>,
+    map: [Dest; 8],
 }
 
 // TODO: mem errors
@@ -33,11 +27,16 @@ impl Memory {
             Mapper::ZX64K => Memory {
                 ram: vec![0; 0x10000],
                 rom: Vec::new(),
-                mappings: vec![Mapping {
-                    start: 0,
-                    end: 0xFFFF,
-                    dest: Dest::RAM,
-                }],
+                map: [
+                    Dest::RAM { start: 0x0000 },
+                    Dest::RAM { start: 0x2000 },
+                    Dest::RAM { start: 0x4000 },
+                    Dest::RAM { start: 0x6000 },
+                    Dest::RAM { start: 0x8000 },
+                    Dest::RAM { start: 0xA000 },
+                    Dest::RAM { start: 0xC000 },
+                    Dest::RAM { start: 0xE000 },
+                ],
             },
             Mapper::SegaGG => Memory {
                 ram: vec![0; 8192],
@@ -45,93 +44,61 @@ impl Memory {
                     Some(rom) => rom.to_vec(),
                     None => Vec::new(),
                 },
-                mappings: vec![
-                    Mapping {
-                        start: 0xC000,
-                        end: 0xDFFF,
-                        dest: Dest::RAM,
-                    },
-                    Mapping {
-                        start: 0xE000,
-                        end: 0xFFFF,
-                        dest: Dest::RAM,
-                    },
+                map: [
+                    Dest::Panic,
+                    Dest::Panic,
+                    Dest::Panic,
+                    Dest::Panic,
+                    Dest::Panic,
+                    Dest::Panic,
+                    Dest::RAM { start: 0x0 },
+                    Dest::RAM { start: 0x0 },
                 ],
             },
         };
         if mem.rom.len() > 0 {
-            mem.mappings.push(Mapping {
-                start: 0,
-                end: 0x3FFF,
-                dest: Dest::ROM {
-                    start: 0,
-                    end: 0x3FFF,
-                },
-            });
+            mem.map[0] = Dest::ROM { start: 0x0000 };
+            mem.map[1] = Dest::ROM { start: 0x2000 };
         } // addititional mappings will be done at runtime
         mem
     }
     pub fn fetch_u8(&self, addr: u16) -> u8 {
-        for mapping in self.mappings.iter() {
-            if addr >= mapping.start && addr <= mapping.end {
-                match mapping.dest {
-                    Dest::ROM { start, end: _ } => {
-                        return self.rom[(addr - start) as usize];
-                    }
-                    Dest::RAM => {
-                        return self.ram[(addr - mapping.start) as usize];
-                    }
-                }
-            }
+        match self.map[addr as usize >> 13] {
+            Dest::ROM { start } => self.rom[((addr & !0xC000) + start) as usize],
+            Dest::RAM { start } => self.ram[((addr & !0xC000).overflowing_add(start).0) as usize],
+            Dest::Panic => panic!("Unmapped memory read: @{:04X}", addr),
         }
-        panic!("Unmapped memory read: @{:04X}", addr);
     }
     pub fn fetch_u16(&self, addr: u16) -> u16 {
         self.fetch_u8(addr) as u16 | ((self.fetch_u8(addr.overflowing_add(1).0) as u16) << 8)
     }
 
     pub fn fetch_range_safe(&self, addr: u16, len: u16) -> &[u8] {
-        for mapping in self.mappings.iter() {
-            if addr >= mapping.start && addr + len - 1 <= mapping.end {
-                match mapping.dest {
-                    Dest::ROM { start, end: _ } => {
-                        return &self.rom[(addr - start) as usize..(addr - start + len) as usize];
-                    }
-                    Dest::RAM => {
-                        return &self.ram[(addr - mapping.start) as usize
-                            ..(addr - mapping.start + len) as usize];
-                    }
-                }
+        match self.map[addr as usize >> 13] {
+            Dest::ROM { start } => {
+                &self.rom
+                    [((addr & !0xC000) + start) as usize..((addr & !0xC000) + start + len) as usize]
             }
+            Dest::RAM { start } => {
+                &self.ram
+                    [((addr & !0xC000) + start) as usize..((addr & !0xC000) + start + len) as usize]
+            }
+            Dest::Panic => panic!("couldn't get a memory slice for @{:04X}+{:04X}", addr, len),
         }
-        panic!("couldn't get a memory slice for @{:04X}+{:04X}", addr, len);
     }
 
     pub fn set_u8(&mut self, addr: u16, val: u8) {
-        for mapping in self.mappings.iter() {
-            if addr >= mapping.start && addr <= mapping.end {
-                match mapping.dest {
-                    Dest::ROM { start: _, end: _ } => {
-                        panic!("Write to ROM address: {:04X}", addr);
-                    }
-                    Dest::RAM => {
-                        self.ram[(addr - mapping.start) as usize] = val;
-                        return;
-                    }
-                }
+        match self.map[addr as usize >> 13] {
+            Dest::ROM { start: _ } => panic!("Write to ROM address: {:04X}", addr),
+            Dest::RAM { start } => {
+                self.ram[((addr & !0xC000).overflowing_add(start).0) as usize] = val
             }
+            Dest::Panic => panic!("Unmapped memory write: @{:04X}: {:02X}", addr, val),
         }
-        panic!("Unmapped memory write: @{:04X}: {:02X}", addr, val);
     }
     pub fn set_u16(&mut self, addr: u16, val: u16) {
         self.set_u8(addr, (val & 0xFF) as u8);
         self.set_u8(addr.overflowing_add(1).0, ((val >> 8) & 0xFF) as u8);
-    }
-    pub fn len(&self) -> usize {
-        self.ram.len()
-    }
-    pub fn is_empty(&self) -> bool {
-        self.ram.is_empty()
     }
 }
 
@@ -143,11 +110,17 @@ impl From<Vec<u8>> for Memory {
         Memory {
             ram: a,
             rom: Vec::new(),
-            mappings: vec![Mapping {
-                start: 0,
-                end: newlen - 1,
-                dest: Dest::RAM,
-            }],
+            map: [
+                // todo: proper implementation based on size
+                Dest::RAM { start: 0x0000 },
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+            ],
         }
     }
 }
@@ -157,18 +130,24 @@ impl Default for Memory {
         Memory {
             ram: vec![0; 8192],
             rom: Vec::new(),
-            mappings: vec![Mapping {
-                start: 0x0000,
-                end: 0x1FFF,
-                dest: Dest::RAM,
-            }],
+            map: [
+                // todo: proper implementation based on size
+                Dest::RAM { start: 0x0000 },
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+                Dest::Panic,
+            ],
         }
     }
 }
 
 impl fmt::Debug for Memory {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(format!("{:?}", self.mappings).as_str())?;
+        f.write_str(format!("{:?}\n", self.map).as_str())?;
         let mut i = 0;
         let mut star = false; // line repeat marker
         while i < self.ram.len() {
@@ -197,5 +176,17 @@ impl fmt::Debug for Memory {
             i += 1;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::mem::*;
+    #[test]
+    fn basic_set() {
+        let mut mem = Memory::init(Mapper::ZX64K, None);
+        mem.set_u8(32768, 19);
+        dbg!(&mem);
+        assert_eq!(mem.fetch_u8(32768), 19);
     }
 }
