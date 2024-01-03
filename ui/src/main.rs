6956a44b0d1b4aa03a1cb26a7dade41c834ff47f
Anisse Astier <anisse@astier.eu>
2024-01-03 22:32:57 +0100

    ui: add web support
    This follows pixels' minimal-web example. Pixels building needs to be
    async, probably from the way wgpu is initialized:
     * main function code is moved to an async function that is waited-on
       with the pollster crate on native, and spawn with
       wasm_bindgen_futures on web
     * Pixels is built with build_async() instead of build()
     * For debugging convenience, the panics are hooked to the console.
    
    Since we don't use logging via the log crate with console_log plugging,
    nothing else will be sent to the javascript console. We will find a
    solution for that later.
    
    We fix wasm-bindgen version, as this needs to be the same as the
    wasm-bindgen-cli installed on the host. Currently we build like this:
    
      env RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release  --target wasm32-unknown-unknown && wasm-bindgen --out-dir web --web ../target/wasm32-unknown-unknown/release/gears-ui.wasm
    
    The minimal index.htm is provided.
    
    No audio support yet, and it panics on thread::sleep().

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -5,11 +5,13 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::rc::Rc;
 
 #[cfg(target_arch = "wasm32")]
 use instant::{Duration, Instant};
 #[cfg(not(target_arch = "wasm32"))]
 use std::time::{Duration, Instant};
+
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use gilrs::Gilrs;
 use pixels::{Pixels, SurfaceTexture};
@@ -22,6 +24,23 @@ use gears::emu;
 use gears::emu::testcmd;
 
 fn main() -> Result<(), String> {
+    #[cfg(target_arch = "wasm32")]
+    {
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+        wasm_bindgen_futures::spawn_local(run_web());
+        Ok(())
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        pollster::block_on(run())
+    }
+}
+#[cfg(target_arch = "wasm32")]
+async fn run_web() {
+    run().await.unwrap();
+}
+async fn run() -> Result<(), String> {
     let event_loop = EventLoop::new();
     let window = {
         let size = LogicalSize::new(emu::LCD_WIDTH as f64, emu::LCD_HEIGHT as f64);
@@ -33,16 +52,63 @@ fn main() -> Result<(), String> {
             .unwrap()
     };
 
+    let window = Rc::new(window);
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowExtWebSys;
+
+        // Retrieve current width and height dimensions of browser client window
+        let get_window_size = || {
+            let client_window = web_sys::window().unwrap();
+            LogicalSize::new(
+                client_window.inner_width().unwrap().as_f64().unwrap(),
+                client_window.inner_height().unwrap().as_f64().unwrap(),
+            )
+        };
+
+        let window = Rc::clone(&window);
+
+        // Initialize winit window with current dimensions of browser client
+        window.set_inner_size(get_window_size());
+
+        let client_window = web_sys::window().unwrap();
+
+        // Attach winit canvas to body element
+        web_sys::window()
+            .and_then(|win| win.document())
+            .and_then(|doc| doc.body())
+            .and_then(|body| {
+                body.append_child(&web_sys::Element::from(window.canvas()))
+                    .ok()
+            })
+            .expect("couldn't append canvas to document body");
+
+        // Listen for resize event on browser client. Adjust winit window dimensions
+        // on event trigger
+        let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
+            let size = get_window_size();
+            window.set_inner_size(size)
+        }) as Box<dyn FnMut(_)>);
+        client_window
+            .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+            .unwrap();
+        closure.forget();
+    }
+
     let mut pixels = {
         let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        let surface_texture =
+            SurfaceTexture::new(window_size.width, window_size.height, window.as_ref());
         pixels::PixelsBuilder::new(
             emu::LCD_WIDTH as u32,
             emu::LCD_HEIGHT as u32,
             surface_texture,
         )
         .present_mode(pixels::wgpu::PresentMode::AutoNoVsync)
-        .build()
+        .build_async()
+        .await
         .map_err(|e| format!("pixels buffer init: {e}"))?
     };
 
