aedf1a23bc728a81e0ca79ed0547d5c394136ce5
Anisse Astier <anisse@astier.eu>
2024-06-29 17:48:07 +0200

    cli: make it work with test commands
    This will make it much easier for reproducible benchmarking

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,28 +1,36 @@
-use gears::emu;
-
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-pub fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file = args.get(1).expect("needs an argument");
-    let path = Path::new(file);
+use gears::emu;
+use gears::emu::testcmd::TestCommand;
 
-    let mut file = match File::open(path) {
-        Err(why) => panic!("Cannot open {}: {}", path.display(), why),
-        Ok(f) => f,
-    };
+fn run_up_to(filename: &Path, cmds: &[TestCommand]) -> Result<u32, String> {
+    let path = Path::new(&filename);
+
+    let mut file =
+        File::open(path).map_err(|why| format!("Cannot open {}: {}", path.display(), why))?;
 
     let mut data: Vec<u8> = vec![];
-    if let Err(why) = file.read_to_end(&mut data) {
-        panic!("Could not read {}: {}", path.display(), why);
-    }
+    file.read_to_end(&mut data)
+        .map_err(|why| format!("Could not read {}: {}", path.display(), why))?;
+    let audio_conf = emu::AudioConf::new(2, 44100)?;
+    let (mut emu, mut audio) = emu::Emulator::init(data, true, audio_conf.clone());
+    let mut pixels = vec![0; emu::LCD_WIDTH * emu::LCD_HEIGHT * 4];
+    let frame = emu.run_commands(&mut pixels, cmds, &mut audio, audio_conf);
+    Ok(frame)
+}
 
-    let (mut emu, _) = emu::Emulator::init(data, false, emu::AudioConf::new(2, 44100).unwrap());
-    let mut pixels = vec![0; 32 * 8 * 28 * 8 * 4];
-    loop {
-        emu.step(&mut pixels);
-    }
+pub fn main() -> Result<(), String> {
+    let args: Vec<String> = env::args().collect();
+    let file = args.get(1).expect("needs a file argument");
+    let path = Path::new(file);
+
+    let cmds = args.get(2).expect("needs gears testcmd argument");
+    let cmds = TestCommand::new_vec(cmds).expect("not a test command format");
+
+    let frame = run_up_to(path, &cmds)?;
+    println!("Ran {file} up to frame {frame}");
+    Ok(())
 }
