03e106d41bab18cbe9d4b97204084f33aeeb7924
Anisse Astier <anisse@astier.eu>
2024-11-04 15:53:24 +0100

    mem: move each mapper implementation to separate structs implementing the same Trait
    Instead of propagating the generic up with State, we use a trait object
    and dynamic dispatch instead.
    
    Criterion says:
         Running benches/mem.rs (target/release/deps/mem-35ff881bbdbd033b)
    mem set                 time:   [1.2520 ns 1.2538 ns 1.2560 ns]
                            change: [-25.265% -24.900% -24.265%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 19 outliers among 100 measurements (19.00%)
      19 (19.00%) high severe
    
    mem set full            time:   [82.123 µs 82.305 µs 82.527 µs]
                            change: [-27.445% -27.236% -27.034%] (p = 0.00 < 0.05)
                            Performance has improved.
    Found 21 outliers among 100 measurements (21.00%)
    
    Getting rid of dynamic dispatch would probably be even faster, but at
    the cost of having State become State<T: MemoryMapper> everywhere.
    
    Game-gear only test with gears-cli shows a 5% global performance impact
    though :-/

--- src/mem.rs
+++ src/mem.rs
@@ -1,149 +1,126 @@
+use std::any::Any;
 use std::fmt;
 
-#[derive(PartialEq, Eq, Clone, Debug)]
-enum Dest {
-    Rom { start: u32 },
-    Ram { start: u16 },
-    Bram { start: u16 },
-    Panic,
-}
-
-#[derive(PartialEq, Eq, Clone)]
-pub enum Mapper {
-    ZX64K, // useful for testing
-    SegaGG {
-        rom: Vec<u8>,
-        backup_ram: Option<Vec<u8>>,
-    },
-    // Leave possibility to add codemasters mapper later
-}
-
-#[derive(PartialEq, Eq, Clone)]
-pub struct Memory {
-    ram: Vec<u8>,
-    mapper: Mapper,
-    map: [Dest; 8],
-}
-
-// TODO: mem errors
-impl Memory {
-    pub fn init(mapper: Mapper) -> Memory {
-        match mapper {
-            Mapper::ZX64K => Memory {
-                ram: vec![0; 0x10000],
-                mapper,
-                map: [
-                    Dest::Ram { start: 0x0000 },
-                    Dest::Ram { start: 0x2000 },
-                    Dest::Ram { start: 0x4000 },
-                    Dest::Ram { start: 0x6000 },
-                    Dest::Ram { start: 0x8000 },
-                    Dest::Ram { start: 0xA000 },
-                    Dest::Ram { start: 0xC000 },
-                    Dest::Ram { start: 0xE000 },
-                ],
-            },
-            Mapper::SegaGG { .. } => Memory {
-                ram: vec![0; 8192],
-                mapper,
-                map: [
-                    Dest::Rom { start: 0x0000 },
-                    Dest::Rom { start: 0x2000 },
-                    Dest::Rom { start: 0x4000 },
-                    Dest::Rom { start: 0x6000 },
-                    Dest::Rom { start: 0x8000 },
-                    Dest::Rom { start: 0xA000 },
-                    Dest::Ram { start: 0x0 },
-                    Dest::Ram { start: 0x0 },
-                ],
-            },
-        }
-    }
-    pub fn fetch_u8(&self, addr: u16) -> u8 {
-        match self.map[addr as usize >> 13] {
-            Dest::Rom { start } => {
-                if let Mapper::SegaGG { rom, .. } = &self.mapper {
-                    rom[(addr & !0xE000) as usize + start as usize]
-                } else {
-                    unreachable!()
-                }
-            }
-            Dest::Ram { start } => self.ram[((addr & !0xE000).wrapping_add(start)) as usize],
-            Dest::Bram { start } => {
-                if let Mapper::SegaGG {
-                    backup_ram: Some(bram),
-                    ..
-                } = &self.mapper
-                {
-                    bram[((addr & !0xE000).wrapping_add(start)) as usize]
-                } else {
-                    unreachable!()
-                }
-            }
-            Dest::Panic => panic!("Unmapped memory read: @{:04X}", addr),
-        }
-    }
-    pub fn fetch_u16(&self, addr: u16) -> u16 {
-        self.fetch_u8(addr) as u16 | ((self.fetch_u8(addr.wrapping_add(1)) as u16) << 8)
-    }
-
-    pub fn fetch_range_safe(&self, addr: u16, dest: &mut [u8]) {
+trait MemoryMapper: EqDyn + CloneDyn + fmt::Debug {
+    fn fetch_u8(&self, addr: u16) -> u8;
+    fn fetch_range_safe(&self, addr: u16, dest: &mut [u8]) {
         for (i, d) in dest.iter_mut().enumerate() {
             *d = self.fetch_u8(addr.wrapping_add(i as u16))
         }
     }
-
-    pub fn set_u8(&mut self, addr: u16, val: u8) {
-        match self.map[addr as usize >> 13] {
-            Dest::Rom { .. } => {
-                println!(
-                    "Ignoring write to ROM address: @{:04X} = {:02X} (old:{:02X})",
-                    addr,
-                    val,
-                    self.fetch_u8(addr)
-                );
-            }
-            Dest::Ram { start } => {
-                if addr >= 0xFFFC {
-                    if let Mapper::SegaGG { .. } = self.mapper {
-                        self.rom_banking(addr, val);
-                    }
-                }
-                self.ram[((addr & !0xE000).wrapping_add(start)) as usize] = val
-            }
-            Dest::Bram { start } => {
-                if let Mapper::SegaGG {
-                    backup_ram: Some(bram),
-                    ..
-                } = &mut self.mapper
-                {
-                    bram[((addr & !0xE000).wrapping_add(start)) as usize] = val
-                } else {
-                    unreachable!()
-                }
-            }
-            Dest::Panic => panic!("Unmapped memory write: @{:04X}: {:02X}", addr, val),
-        }
+    fn fetch_u16(&self, addr: u16) -> u16 {
+        self.fetch_u8(addr) as u16 | ((self.fetch_u8(addr.wrapping_add(1)) as u16) << 8)
     }
-    pub fn set_u16(&mut self, addr: u16, val: u16) {
+    fn set_u8(&mut self, addr: u16, val: u8);
+    fn set_u16(&mut self, addr: u16, val: u16) {
         self.set_u8(addr, (val & 0xFF) as u8);
         self.set_u8(addr.wrapping_add(1), ((val >> 8) & 0xFF) as u8);
     }
+}
 
-    fn rom_bitmask(&self) -> u32 {
-        if let Mapper::SegaGG { rom, .. } = &self.mapper {
-            return match rom.len() {
-                32768 => 0x00007FFF,
-                65536 => 0x0000FFFF,
-                131072 => 0x0001FFFF,
-                262144 => 0x0003FFFF,
-                524288 => 0x0007FFFF,
-                1048576 => 0x000FFFFF,
-                // Technically this should be unsupported, but keep it as-is for now
-                _ => 0xFFFFFFFF,
-            };
+trait EqDyn: Any {
+    fn eq_dyn(&self, other: &dyn Any) -> bool;
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl<T: Eq + 'static> EqDyn for T {
+    fn eq_dyn(&self, other: &dyn Any) -> bool {
+        other.downcast_ref().map_or(false, |o| self == o)
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+impl PartialEq for dyn MemoryMapper {
+    fn eq(&self, other: &Self) -> bool {
+        self.eq_dyn(other.as_any())
+    }
+}
+impl Eq for Box<dyn MemoryMapper> {}
+
+impl PartialEq<&Self> for Box<dyn MemoryMapper> {
+    fn eq(&self, other: &&Self) -> bool {
+        self == *other
+    }
+}
+
+trait CloneDyn {
+    fn clone_dyn(&self) -> Box<dyn MemoryMapper>;
+}
+
+impl<T: MemoryMapper + Clone> CloneDyn for T {
+    fn clone_dyn(&self) -> Box<dyn MemoryMapper> {
+        Box::new(std::clone::Clone::clone(self))
+    }
+}
+
+#[derive(PartialEq, Eq, Clone)]
+struct ZX64kMapper {
+    ram: [u8; 0x10000],
+}
+impl Default for ZX64kMapper {
+    fn default() -> Self {
+        Self { ram: [0; 0x10000] }
+    }
+}
+
+impl MemoryMapper for ZX64kMapper {
+    fn fetch_u8(&self, addr: u16) -> u8 {
+        self.ram[addr as usize]
+    }
+
+    fn set_u8(&mut self, addr: u16, val: u8) {
+        self.ram[addr as usize] = val;
+    }
+}
+
+impl From<Vec<u8>> for ZX64kMapper {
+    fn from(init: Vec<u8>) -> Self {
+        let mut m = Self::default();
+        //let range_dst = 0..init.len().max(m.ram.len());
+        let range = 0..init.len().min(m.ram.len());
+        m.ram[range.clone()].copy_from_slice(&init[range]);
+        m
+    }
+}
+
+#[derive(PartialEq, Eq, Clone)]
+struct SegaGGMapper {
+    ram: [u8; 8192],
+    map: [Dest; 8],
+    rom: Vec<u8>,
+    backup_ram: Option<Vec<u8>>,
+}
+
+impl SegaGGMapper {
+    fn new(rom: Vec<u8>, backup_ram: Option<Vec<u8>>) -> Self {
+        Self {
+            ram: [0; 8192],
+            map: [
+                Dest::Rom { start: 0x0000 },
+                Dest::Rom { start: 0x2000 },
+                Dest::Rom { start: 0x4000 },
+                Dest::Rom { start: 0x6000 },
+                Dest::Rom { start: 0x8000 },
+                Dest::Rom { start: 0xA000 },
+                Dest::Ram { start: 0x0 },
+                Dest::Ram { start: 0x0 },
+            ],
+            rom,
+            backup_ram,
         }
-        0xFFFFFFFF
+    }
+    fn rom_bitmask(&self) -> u32 {
+        match self.rom.len() {
+            32768 => 0x00007FFF,
+            65536 => 0x0000FFFF,
+            131072 => 0x0001FFFF,
+            262144 => 0x0003FFFF,
+            524288 => 0x0007FFFF,
+            1048576 => 0x000FFFFF,
+            // Technically this should be unsupported, but keep it as-is for now
+            _ => 0xFFFFFFFF,
+        }
     }
 
     fn set_bank(&mut self, num: usize, val: u8) {
@@ -163,16 +140,12 @@ impl Memory {
                 /* BANK CONTROL REGISTER */
                 if val & 0x08 != 0 {
                     /* Select Backup RAM */
-                    if let Mapper::SegaGG { backup_ram, .. } = &mut self.mapper {
-                        if backup_ram.is_none() {
-                            backup_ram.replace(vec![0; 8192]);
-                        }
-                        if backup_ram.is_some() {
-                            self.map[4] = Dest::Bram { start: 0x0000 };
-                            self.map[5] = Dest::Bram { start: 0x0000 };
-                        } else {
-                            unreachable!();
-                        }
+                    if self.backup_ram.is_none() {
+                        self.backup_ram.replace(vec![0; 8192]);
+                    }
+                    if self.backup_ram.is_some() {
+                        self.map[4] = Dest::Bram { start: 0x0000 };
+                        self.map[5] = Dest::Bram { start: 0x0000 };
                     } else {
                         unreachable!();
                     }
@@ -204,80 +177,168 @@ impl Memory {
     }
 }
 
-impl From<Vec<u8>> for Memory {
-    fn from(mut a: Vec<u8>) -> Self {
-        let len = a.len();
-        let newlen = (len as u16 + 3) & 0xFFFC; // this is used for testing, give us at least one instruction
-        a.resize(newlen as usize, 0);
-        Memory {
-            ram: a,
-            mapper: Mapper::ZX64K,
-            map: [
-                // todo: proper implementation based on size
-                Dest::Ram { start: 0x0000 },
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-            ],
+impl MemoryMapper for SegaGGMapper {
+    fn fetch_u8(&self, addr: u16) -> u8 {
+        match self.map[addr as usize >> 13] {
+            Dest::Rom { start } => self.rom[(addr & !0xE000) as usize + start as usize],
+            Dest::Ram { start } => self.ram[((addr & !0xE000).wrapping_add(start)) as usize],
+            Dest::Bram { start } => {
+                if let Some(bram) = &self.backup_ram {
+                    bram[((addr & !0xE000).wrapping_add(start)) as usize]
+                } else {
+                    // We should never put bram in the map if there is no backup ram
+                    unreachable!()
+                }
+            }
+            Dest::Panic => panic!("Unmapped memory read: @{:04X}", addr),
+        }
+    }
+
+    fn set_u8(&mut self, addr: u16, val: u8) {
+        match self.map[addr as usize >> 13] {
+            Dest::Rom { .. } => {
+                println!(
+                    "Ignoring write to ROM address: @{:04X} = {:02X} (old:{:02X})",
+                    addr,
+                    val,
+                    self.fetch_u8(addr)
+                );
+            }
+            Dest::Ram { start } => {
+                if addr >= 0xFFFC {
+                    self.rom_banking(addr, val);
+                }
+                self.ram[((addr & !0xE000).wrapping_add(start)) as usize] = val
+            }
+            Dest::Bram { start } => {
+                if let Some(bram) = &mut self.backup_ram {
+                    bram[((addr & !0xE000).wrapping_add(start)) as usize] = val
+                } else {
+                    // We should never put bram in the map if there is no backup ram
+                    unreachable!()
+                }
+            }
+            Dest::Panic => panic!("Unmapped memory write: @{:04X}: {:02X}", addr, val),
         }
     }
 }
 
+#[derive(PartialEq, Eq, Clone, Debug)]
+enum Dest {
+    Rom { start: u32 },
+    Ram { start: u16 },
+    Bram { start: u16 },
+    Panic,
+}
+
+#[derive(PartialEq, Eq, Clone)]
+pub enum Mapper {
+    ZX64K, // useful for testing
+    SegaGG {
+        rom: Vec<u8>,
+        backup_ram: Option<Vec<u8>>,
+    },
+    // Leave possibility to add codemasters mapper later
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Memory {
+    mapper: Box<dyn MemoryMapper>,
+}
+
+impl Clone for Memory {
+    fn clone(&self) -> Self {
+        Self {
+            mapper: self.mapper.clone_dyn(),
+        }
+    }
+}
+
+impl Memory {
+    pub fn init(mapper: Mapper) -> Memory {
+        match mapper {
+            Mapper::ZX64K => Memory {
+                mapper: Box::new(ZX64kMapper::default()),
+            },
+            Mapper::SegaGG { rom, backup_ram } => Memory {
+                mapper: Box::new(SegaGGMapper::new(rom, backup_ram)),
+            },
+        }
+    }
+    pub fn fetch_u8(&self, addr: u16) -> u8 {
+        self.mapper.fetch_u8(addr)
+    }
+    pub fn fetch_u16(&self, addr: u16) -> u16 {
+        self.mapper.fetch_u16(addr)
+    }
+
+    pub fn fetch_range_safe(&self, addr: u16, dest: &mut [u8]) {
+        self.mapper.fetch_range_safe(addr, dest)
+    }
+
+    pub fn set_u8(&mut self, addr: u16, val: u8) {
+        self.mapper.set_u8(addr, val)
+    }
+    pub fn set_u16(&mut self, addr: u16, val: u16) {
+        self.mapper.set_u16(addr, val)
+    }
+}
+
+impl From<Vec<u8>> for Memory {
+    fn from(value: Vec<u8>) -> Self {
+        Memory {
+            mapper: Box::new(ZX64kMapper::from(value)),
+        }
+    }
+}
 impl Default for Memory {
     fn default() -> Self {
         Memory {
-            ram: vec![0; 8192],
-            mapper: Mapper::ZX64K,
-            map: [
-                // todo: proper implementation based on size
-                Dest::Ram { start: 0x0000 },
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-                Dest::Panic,
-            ],
+            mapper: Box::new(ZX64kMapper::default()),
         }
     }
 }
 
-impl fmt::Debug for Memory {
+fn dbg_mem(block: &[u8], f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    let mut i = 0;
+    let mut star = false; // line repeat marker
+    while i < block.len() {
+        let v = block[i];
+        if i % 16 == 0 {
+            if i > 0 {
+                if i >= 16 && block.len() > i + 16 {
+                    if block[i - 16..i] == block[i..i + 16] {
+                        if !star {
+                            f.write_str("\n*")?;
+                        }
+                        i += 16;
+                        star = true;
+                        continue;
+                    }
+                    star = false;
+                }
+                f.write_str("\n")?;
+            }
+            f.write_str(&format!("{:04X}", i))?;
+        }
+        if i % 8 == 0 {
+            f.write_str(" ")?;
+        }
+        f.write_str(&format!("{:02X} ", v))?;
+        i += 1;
+    }
+    Ok(())
+}
+
+impl fmt::Debug for ZX64kMapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        dbg_mem(&self.ram, f)
+    }
+}
+impl fmt::Debug for SegaGGMapper {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(format!("{:?}\n", self.map).as_str())?;
-        let mut i = 0;
-        let mut star = false; // line repeat marker
-        while i < self.ram.len() {
-            let v = self.ram[i];
-            if i % 16 == 0 {
-                if i > 0 {
-                    if i >= 16 && self.ram.len() > i + 16 {
-                        if self.ram[i - 16..i] == self.ram[i..i + 16] {
-                            if !star {
-                                f.write_str("\n*")?;
-                            }
-                            i += 16;
-                            star = true;
-                            continue;
-                        }
-                        star = false;
-                    }
-                    f.write_str("\n")?;
-                }
-                f.write_str(&format!("{:04X}", i))?;
-            }
-            if i % 8 == 0 {
-                f.write_str(" ")?;
-            }
-            f.write_str(&format!("{:02X} ", v))?;
-            i += 1;
-        }
-        Ok(())
+        dbg_mem(&self.ram, f)
     }
 }
 
